a1fffa9e97a9fe1e6fd14d40c854d446c90e64c1
zehwoua <zehwoua@hotmail.com>
2013-08-28 12:22:43 +0900

    init

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require_tree .
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require fancybox
 //= require rails.validations
-//= require_tree .
 // jQuery.ajaxSetup({ 
 //   'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
 // })
