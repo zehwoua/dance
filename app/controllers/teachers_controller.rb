318fa46e1cc9c1485bd94b012985a5319022b5b6
zehwoua <zehwoua@hotmail.com>
2013-08-30 15:08:43 +0900

    teachers page

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,10 +1,16 @@
 class TeachersController < ApplicationController
   def show
-    @teacher = Teacher.find(params[:id])
+  	@current_teacher_id = params[:id]
+  	@q = Video.search(params[:q])
+  	@videos = @q.result(distinct: true).page(params[:page]).per_page(3).where(:teacher_id => @current_teacher_id)
+   	@level_value = ["1","2","3"]
+
+    @teacher = Teacher.find(@current_teacher_id)
 
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @teacher }
+      format.js
     end
   end
 
