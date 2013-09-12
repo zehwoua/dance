22d743e31918a8b0791033087684af1a8e8ada4b
zehwoua <zehwoua@gmail.com>
2013-09-13 08:41:31 +0900

    admin js

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,1 +1,4 @@
 //= require_tree ./admin
+var clientId = '1064062597356.apps.googleusercontent.com';
+var apiKey = 'AIzaSyDPpGs5UHCAzi7zaB4b65bI0KB_nTE1E5w';
+var scopes = 'https://www.googleapis.com/auth/analytics.readonly';
--- app/assets/javascripts/admin/ga_auth.js
+++ app/assets/javascripts/admin/ga_auth.js
@@ -5,6 +5,3 @@
  * section in the Google JavaScript Client Library.
  * https://code.google.com/p/google-api-javascript-client/wiki/Authentication
  */
-var clientId = '1064062597356.apps.googleusercontent.com';
-var apiKey = 'AIzaSyDPpGs5UHCAzi7zaB4b65bI0KB_nTE1E5w';
-var scopes = 'https://www.googleapis.com/auth/analytics.readonly';
