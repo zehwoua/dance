c44eab816d3217f8d5e41e5e08f2a18c74d2951d
zehwoua <zehwoua@gmail.com>
2013-09-12 12:45:09 +0900

    omn twitter 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ DanceDynamics::Application.routes.draw do
 
   devise_for :users, :controllers => { 
     :registrations => "users/registrations" 
-    :omniauth_callbacks => "users/omniauth_callbacks"
+    
   }
   devise_scope :user do
     get '/sign_in' => 'sessions#new'
@@ -12,6 +12,9 @@ DanceDynamics::Application.routes.draw do
   devise_for :users do
     get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
   end
+  # devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+
   
   # devise_for :users, :path => '', :path_names => 
   #           {:sign_in => '/login',
