0224f25b2fc28abe8137ac79613a7f6a9fa38dc7
zehwoua <zehwoua@gmail.com>
2013-09-12 13:45:14 +0900

    Facebook error

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -9,5 +9,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to new_user_registration_url
     end
   end
-  alias_method :twitter,:facebook, :all
+  alias_method :twitter, :all
+  alias_method :facebook, :all
+
 end
