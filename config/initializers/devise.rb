c4cf0318282d83a206b53d4371658965f7f30daa
zehwoua <zehwoua@gmail.com>
2013-09-12 12:25:32 +0900

    twitter config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -227,7 +227,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
 
-  config.omniauth :twitter, ENV["PqG3sD8Hfv87sT58FBevQ"], ENV["FHw3JiThnVOSL24KLTnrYlQ3lawnBS9oQ5srJtxk4"]
+  config.omniauth :twitter, ENV["TWITTER_CONSUMER_KEY"], ENV["TWITTER_CONSUMER_SECRET"]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
