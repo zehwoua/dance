2232bac7176ae1bc730de0234b4b66824c1a1926
zehwoua <zehwoua@gmail.com>
2013-09-13 08:24:04 +0900

    js content_for header

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,8 @@ DanceDynamics::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.assets.precompile += %w( admin.js )
   
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
