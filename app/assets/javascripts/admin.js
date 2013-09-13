2abf7e1205eb73485303b5ef245563b7fd28042b
zehwoua <zehwoua@gmail.com>
2013-09-13 09:08:06 +0900

    latest comments

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,4 +1,1 @@
 //= require_tree ./admin
-var clientId = '1064062597356.apps.googleusercontent.com';
-var apiKey = 'AIzaSyDPpGs5UHCAzi7zaB4b65bI0KB_nTE1E5w';
-var scopes = 'https://www.googleapis.com/auth/analytics.readonly';
--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,6 +1,7 @@
 class Admin::AdminController < ApplicationController
   def index
   	@users = (current_user.blank? ? User.all : User.find(:all, :conditions => ["id != ?", current_user.id]))
+  	@latest_comments = Comment.latest_comment
   end
 
   def plans
