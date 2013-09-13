90416c6b99328b53831f2c34340161052437e2c4
zehwoua <zehwoua@gmail.com>
2013-09-13 19:20:15 +0900

    js

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -2,11 +2,11 @@ module AdminHelper
 	def load_javascript
 	  if Rails.env.production?
 	  	content_for :header do
-  			javascript_include_tag 'ga_auth_pro', :cache => true 
+  			javascript_include_tag 'ga_auth_pro.js', :cache => true 
   		end
 	  else
 	  	content_for :header do
-  			javascript_include_tag 'ga_auth_dev'
+  			javascript_include_tag 'ga_auth_dev.js'
   		end
 	  end
 	end
