461e7fb572faa3ecdb5fe44097515575cca4709e
zehwoua <zehwoua@gmail.com>
2013-09-13 17:40:28 +0900

    admin users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,67 +3,56 @@ class Admin::UsersController < ApplicationController
 		@search = User.search(params[:q])
     	@users = @search.result
 	end
- #  	def show
-	#   	@current_level_id = params[:id]
-	#   	@q = Video.search(params[:q])
-	#   	@videos = @q.result(distinct: true).page(params[:page]).per_page(3).where(:level_id => @current_level_id)
-	#     @level = Level.find(@current_level_id)
+ 
 
-	#     respond_to do |format|
-	#       	format.html # show.html.erb
-	#       	format.json { render json: @level }
-	#       	format.js
-	#     end
- #  	end
+	def new
+	  	@user = User.new
 
-	# def new
-	#   	@level = Level.new
+	    respond_to do |format|
+	      format.html # new.html.erb
+	      format.json { render json: @user }
+	    end
+	end
 
-	#     respond_to do |format|
-	#       format.html # new.html.erb
-	#       format.json { render json: @level }
-	#     end
-	# end
+	def edit
+	    @user = User.find(params[:id])
+	end
 
-	# def edit
-	#     @level = Level.find(params[:id])
-	# end
+  	def create
+	    @user = User.new(params[:user])
 
- #  	def create
-	#     @level = Level.new(params[:level])
+	    respond_to do |format|
+		    if @user.save
+		        format.html { redirect_to admin_users_path, notice: 'user was successfully created.' }
+		        format.json { render json: @user, status: :created, location: @teacher }
+		    else
+		        format.html { render action: "new" }
+		        format.json { render json: @user.errors, status: :unprocessable_entity }
+	      	end
+	    end
+  	end
 
-	#     respond_to do |format|
-	# 	    if @level.save
-	# 	        format.html { redirect_to admin_levels_path, notice: 'level was successfully created.' }
-	# 	        format.json { render json: @level, status: :created, location: @teacher }
-	# 	    else
-	# 	        format.html { render action: "new" }
-	# 	        format.json { render json: @level.errors, status: :unprocessable_entity }
-	#       	end
-	#     end
- #  	end
+  	def update
+    	@user = User.find(params[:id])
 
- #  	def update
- #    	@level = Level.find(params[:id])
+	    respond_to do |format|
+	      	if @user.update_attributes(params[:user])
+		        format.html { redirect_to admin_users_path, notice: 'user was successfully updated.' }
+		        format.json { head :no_content }
+	      	else
+		        format.html { render action: "edit" }
+		        format.json { render json: @user.errors, status: :unprocessable_entity }
+	      	end
+	    end
+  	end
 
-	#     respond_to do |format|
-	#       	if @level.update_attributes(params[:level])
-	# 	        format.html { redirect_to admin_levels_path, notice: 'level was successfully updated.' }
-	# 	        format.json { head :no_content }
-	#       	else
-	# 	        format.html { render action: "edit" }
-	# 	        format.json { render json: @level.errors, status: :unprocessable_entity }
-	#       	end
-	#     end
- #  	end
+  	def destroy
+	    @user = User.find(params[:id])
+	    @user.destroy
 
- #  	def destroy
-	#     @level = Level.find(params[:id])
-	#     @level.destroy
-
-	#     respond_to do |format|
-	# 	    format.html { redirect_to admin_levels_path }
-	# 	    format.json { head :no_content }
-	#     end
- #  	end
+	    respond_to do |format|
+		    format.html { redirect_to admin_users_path }
+		    format.json { head :no_content }
+	    end
+  	end
   end
